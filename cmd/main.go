e426fbdab83a2b8a06974c901fefaf0dc79fba24
Джигкаев Максим Тамерланович <Dzhigkaev.Maksim@wildberries.ru>
2020-06-18 22:23:26 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
-	"runtime"
 	"syscall"
 
 	"github.com/maxim-dzh/worker-pool/pkg/image"
@@ -13,26 +11,26 @@ import (
 )
 
 const (
-	chanLen = 100
+	chanLen  = 100
+	routines = 100
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	signals := make(chan os.Signal, 1)
-	go func() {
-		signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
-		<-signals
-		cancel()
-	}()
+	defer cancel()
 
-	p := pool.NewPool(
-		ctx,
-		runtime.GOMAXPROCS(0),
-		chanLen,
-	)
+	p := pool.NewPool(ctx, routines, chanLen)
+	defer p.Close()
+	go p.Start(ctx)
+
 	queue := p.Queue()
 	for i := 0; i < chanLen; i++ {
 		queue <- image.NewImage()
 	}
-	fmt.Scanln()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
+	defer func(sig os.Signal) {
+		cancel()
+	}(<-signals)
 }
--- pkg/pool/pool.go
+++ pkg/pool/pool.go
@@ -8,11 +8,25 @@ type job interface {
 
 // Pool ...
 type Pool interface {
+	Start(ctx context.Context)
 	Queue() (queue chan job)
+	Close()
 }
 
 type pool struct {
-	queue chan job
+	queue       chan job
+	maxRoutines int
+}
+
+// Start ...
+func (p *pool) Start(ctx context.Context) {
+	for i := 0; i < p.maxRoutines; i++ {
+		go func(ctx context.Context) {
+			for job := range p.queue {
+				job.Process(ctx)
+			}
+		}(ctx)
+	}
 }
 
 // Queue ...
@@ -20,16 +34,15 @@ func (p *pool) Queue() (queue chan job) {
 	return p.queue
 }
 
+// Close ...
+func (p *pool) Close() {
+	close(p.queue)
+}
+
 // NewPool ...
 func NewPool(ctx context.Context, maxRoutines, chanLen int) Pool {
-	var p pool
-	p.queue = make(chan job, chanLen)
-	for i := 0; i < maxRoutines; i++ {
-		go func(ctx context.Context) {
-			for job := range p.queue {
-				job.Process(ctx)
-			}
-		}(ctx)
+	return &pool{
+		queue:       make(chan job, chanLen),
+		maxRoutines: maxRoutines,
 	}
-	return &p
 }
